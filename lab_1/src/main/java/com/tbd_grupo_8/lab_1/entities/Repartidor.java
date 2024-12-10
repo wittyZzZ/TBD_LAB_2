0493d96c857da2bf1b6eddfc6c56add385d01c3f
fcr1s <cristobal.floresagui@gmail.com>
2024-12-10 17:40:38 -0300

    Create Repartidor.java

---
+++ lab_1/src/main/java/com/tbd_grupo_8/lab_1/entities/Repartidor.java
@@ -0,0 +1,16 @@
+package com.tbd_grupo_8.lab_1.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Repartidor {
+    private Long id_repartidor;
+    private String nombre;
+    private Point coordenadas;
+}
