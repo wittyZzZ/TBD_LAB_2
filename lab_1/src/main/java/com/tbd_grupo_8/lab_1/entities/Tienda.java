23c39072954475093f37746f057de840d56f18c7
fcr1s <cristobal.floresagui@gmail.com>
2024-12-10 17:40:42 -0300

    Create Tienda.java

---
+++ lab_1/src/main/java/com/tbd_grupo_8/lab_1/entities/Tienda.java
@@ -0,0 +1,17 @@
+package com.tbd_grupo_8.lab_1.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tienda {
+    private Long id_tienda;
+    private String nombre;
+    private String direccion;
+    private Point coordenadas;
+}
